dba150b21e40153b9f83de6d3a79b498963f4470
Dmitry Averin <avedmigen@gmail.com>
2021-01-15 17:48:19 +0300

    Откатывает изменения в презентере карточки

--- src/presenter/movie.js
+++ src/presenter/movie.js
@@ -24,9 +24,9 @@ export default class Movie {
     this._handlePopupCloseButtonClick = this._handlePopupCloseButtonClick.bind(this);
     this._handleEscKeyDown = this._handleEscKeyDown.bind(this);
 
-    this._handlePopupWatchlistClick = this._handlePopupWatchlistClick.bind(this);
-    this._handlePopupWatchedClick = this._handlePopupWatchedClick.bind(this);
-    this._handlePopupFavoritesClick = this._handlePopupFavoritesClick.bind(this);
+    this._handleWatchlistClick = this._handleWatchlistClick.bind(this);
+    this._handleWatchedClick = this._handleWatchedClick.bind(this);
+    this._handleFavoritesClick = this._handleFavoritesClick.bind(this);
 
   }
 
@@ -46,9 +46,9 @@ export default class Movie {
     this._cardComponent.setWatchedClickHandler(this._handleWatchedClick);
     this._cardComponent.setFavoritesClickHandler(this._handleFavoritesClick);
 
-    this._popupComponent.setPopupWatchlistClickHandler(this._handlePopupWatchlistClick);
-    this._popupComponent.setPopupWatchedClickHandler(this._handlePopupWatchedClick);
-    this._popupComponent.setPopupFavoritesClickHandler(this._handlePopupFavoritesClick);
+    this._popupComponent.setPopupWatchlistClickHandler(this._handleWatchlistClick);
+    this._popupComponent.setPopupWatchedClickHandler(this._handleWatchedClick);
+    this._popupComponent.setPopupFavoritesClickHandler(this._handleFavoritesClick);
 
     if (prevCardComponent === null || prevPopupComponent === null) {
       render(this._moviesListContainer, this._cardComponent, RenderPosition.AFTERBEGIN);
@@ -139,19 +139,8 @@ export default class Movie {
     );
   }
 
-
   _handlePopupCloseButtonClick() {
     this._replacePopupToCard();
-  }
-
-
-  _handlePopupWatchlistClick() {
-  }
-
-  _handlePopupWatchedClick() {
-  }
-
-  _handlePopupFavoritesClick() {
   }
 
 }
