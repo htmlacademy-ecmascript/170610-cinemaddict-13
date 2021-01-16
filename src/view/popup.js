ab83b7a558bf6dedccbf852d0ae36bb2171aab00
Dmitry Averin <avedmigen@gmail.com>
2021-01-16 16:13:01 +0300

    Изменяет активные классы на чекбоксах в попапе

--- src/view/popup.js
+++ src/view/popup.js
@@ -3,7 +3,7 @@ import AbstractView from "./abstract.js";
 
 const createMovieDetailsPopupTemplate = (film) => {
 
-  const {title, date, rating, duration, genre, poster, description, director, writers, actors, country, ageRating} = film;
+  const {title, date, rating, duration, genre, poster, description, director, writers, actors, country, ageRating, isWatchlist, isWatched, isFavorite,} = film;
 
   return `<section class="film-details">
   <form class="film-details__inner" action="" method="get">
@@ -71,13 +71,13 @@ const createMovieDetailsPopupTemplate = (film) => {
       </div>
 
       <section class="film-details__controls">
-        <input type="checkbox" class="film-details__control-input visually-hidden" id="watchlist" name="watchlist">
+        <input type="checkbox" ${ isWatchlist ? `checked` : ``} class="film-details__control-input visually-hidden" id="watchlist" name="watchlist">
         <label for="watchlist" class="film-details__control-label film-details__control-label--watchlist">Add to watchlist</label>
 
-        <input type="checkbox" class="film-details__control-input visually-hidden" id="watched" name="watched">
+        <input type="checkbox" ${ isWatched ? `checked` : ``} class="film-details__control-input visually-hidden" id="watched" name="watched">
         <label for="watched" class="film-details__control-label film-details__control-label--watched">Already watched</label>
 
-        <input type="checkbox" class="film-details__control-input visually-hidden" id="favorite" name="favorite">
+        <input type="checkbox" ${ isFavorite ? `checked` : ``} class="film-details__control-input visually-hidden" id="favorite" name="favorite">
         <label for="favorite" class="film-details__control-label film-details__control-label--favorite">Add to favorites</label>
       </section>
     </div>
